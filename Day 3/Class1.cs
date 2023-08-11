23638176f2839a310e8f622a76b6e30b09466ae1
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-08-11 23:59:12 +0300

    value vs reference passing to methods

--- Day 3/Class1.cs
+++ Day 3/Class1.cs
@@ -14,43 +14,69 @@ namespace Day_3
             // System.Console.WriteLine("Fun Two");
 
 
-            StackTrace sTrace = new StackTrace();
+            // StackTrace sTrace = new StackTrace();
 
-            StackFrame[] sFrames = sTrace.GetFrames();
+            // StackFrame[] sFrames = sTrace.GetFrames();
 
-            for (int i = 0; i < sFrames.Length; i++)
-            {
-                System.Console.WriteLine(sFrames[i].GetMethod().Name);
-            }
+            // for (int i = 0; i < sFrames.Length; i++)
+            // {
+            //     System.Console.WriteLine(sFrames[i].GetMethod().Name);
+            // }
 
         }
 
-        public static void PrintLine()
+        public static void PrintLine(int n = 5, string pattern = "x")
         {
             FunOne();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < n; i++)
             {
-                System.Console.Write("#");
+                System.Console.Write(pattern);
             }
             System.Console.WriteLine("");
         }
 
         public static void Main()
         {
-            int x;
-            System.Console.WriteLine("Enter new value: ");
+            // int x;
+            // System.Console.WriteLine("Enter new value: ");
 
-            PrintLine();
+            // PrintLine(3, "-_-");
 
-            x = int.Parse(Console.ReadLine());
+            // // x = int.Parse(Console.ReadLine());
 
-            System.Console.WriteLine(++x);
+            // // System.Console.WriteLine(++x);
 
-            PrintLine();
+            // PrintLine(5, "#");
 
-            System.Console.WriteLine(++x);
+            // // System.Console.WriteLine(++x);
 
 
+            // // You can also reverse the order of passed parameters using this syntax:
+
+            // PrintLine(pattern: "| ", n: 4);
+
+            // PrintLine(pattern: "& "); // n is 5 by default
+
+
+            int a = 7;
+            int b = 3;
+
+            Swap(a, b);
+
+            System.Console.WriteLine($"A: {a}");
+            System.Console.WriteLine($"B: {b}");
+
+            // Doesnt do anything as the a and b are passed by value so they arent actually reversed
+
+
+
+
+        }
+        public static void Swap(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
 
         }
     }
